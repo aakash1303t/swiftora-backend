bb024768156db23824c5ce0544ba5589df2644d8
aakash1303t <aakash93610t@gmail.com>
2025-03-03 10:34:45 +0530

    update-7

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.js";
+import { User } from "../models/User.js";
 
 // Register User
 export const registerUser = async (req, res) => {
